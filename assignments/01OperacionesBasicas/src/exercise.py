38b4f14d4a56d7fb5fa607d6da96a392f00a5e9f
AlanCoronaaa <a01642342@tec.mx>
2021-08-19 22:50:10 +0000

    Envio1

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
+    num1 = int(input("Dame un número: "))
+    num2 = int(input("Dame un número: "))
+    resultado_suma =  num1 + num2
+    resultado_resta = num1 - num2
+    multi = num1 * num2
+    print("Suma:",resultado_suma)
+    print("Resta:",resultado_resta)
+    print("Multiplicación:", multi)
+
+    
+
 
 
 
